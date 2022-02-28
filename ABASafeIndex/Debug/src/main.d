98a4e5ee02a703827c0b7aea0dee52709a26552e
GerdHirsch <kontakt@gerdhirsch.de>
2022-02-28 18:28:27 +0100

    remove unwanted, rename project

--- ABASafeIndex/Debug/src/main.d
+++
@@ -1,7 +0,0 @@
-src/main.o: ../src/main.cpp \
- D:\DevProjects\Cpp\Multithreading\ABASafeIndex\include/ABASafeIndex/ABASafeIndex.h \
- D:\DevProjects\Cpp\Multithreading\ABASafeIndex\include/ABASafeIndex/CalculaterHelper.hpp
-
-D:\DevProjects\Cpp\Multithreading\ABASafeIndex\include/ABASafeIndex/ABASafeIndex.h:
-
-D:\DevProjects\Cpp\Multithreading\ABASafeIndex\include/ABASafeIndex/CalculaterHelper.hpp:
